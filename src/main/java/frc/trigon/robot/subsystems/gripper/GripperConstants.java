746781e5050376b83d3cdc59439fa7094e4bb19b
noamgreen12 <noamarielgreen@gmail.com>
2025-01-10 02:01:21 +0200

    added a LaserCan with its constants

--- src/main/java/frc/trigon/robot/subsystems/gripper/GripperConstants.java
+++ src/main/java/frc/trigon/robot/subsystems/gripper/GripperConstants.java
@@ -1,5 +1,7 @@
 package frc.trigon.robot.subsystems.gripper;
 
+import au.grapplerobotics.ConfigurationFailedException;
+import au.grapplerobotics.LaserCan;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
@@ -11,22 +13,34 @@ import org.trigon.utilities.mechanisms.SpeedMechanism2d;
 
 public class GripperConstants {
     private static final int
-            MOTOR_ID = 14;
+            MOTOR_ID = 14,
+            LASER_CAN_ID = 15;
 
     static final TalonFXMotor MOTOR = new TalonFXMotor(MOTOR_ID, "GripperMotor");
+    static final LaserCan LASER_CAN = new LaserCan(LASER_CAN_ID);
+
+    private static final int
+            NUMBER_OF_MOTORS = 1,
+            X = 8,
+            Y = 8,
+            W = 16,
+            H = 16;
+
 
     private static final InvertedValue INVERTED_VALUE = InvertedValue.CounterClockwise_Positive;
     private static final NeutralModeValue NEUTRAL_MODE_VALUE = NeutralModeValue.Brake;
+    private static final LaserCan.RangingMode LASER_CAN_RANGING_MODE = LaserCan.RangingMode.SHORT;
+    private static final LaserCan.TimingBudget LASER_CAN_TIMING_BUDGET = LaserCan.TimingBudget.TIMING_BUDGET_33MS;
+    private static final LaserCan.RegionOfInterest LASER_CAN_REGION_OF_INTEREST = new LaserCan.RegionOfInterest(X, Y, W, H);
+    private static final LaserCan.Measurement LASER_CAN_MEASUREMENT = LASER_CAN.getMeasurement();
 
     private static final double
-            P = 1,
-            I = 1,
-            D = 1,
-            GEAR_RATIO = 0.5,
-            MOMENT_OF_INERTIA = 0.003,
-            MAXIMUM_DISPLAYABLE_VELOCITY = 12;
-
-    private static final int NUMBER_OF_MOTORS = 1;
+    P = 1,
+    I = 1,
+    D = 1,
+    GEAR_RATIO = 0.5,
+    MOMENT_OF_INERTIA = 0.003,
+    MAXIMUM_DISPLAYABLE_VELOCITY = 12;
 
     static final boolean FOC_ENABLED = true;
     private static final DCMotor GEAR_BOX = DCMotor.getKrakenX60(NUMBER_OF_MOTORS);
@@ -35,6 +49,7 @@ public class GripperConstants {
 
     static {
         configureMotor();
+        configureLaser();
     }
 
     private static void configureMotor() {
@@ -51,6 +66,17 @@ public class GripperConstants {
         MOTOR.registerSignal(TalonFXSignal.POSITION, 100);
         MOTOR.registerSignal(TalonFXSignal.VELOCITY, 100);
         MOTOR.registerSignal(TalonFXSignal.MOTOR_VOLTAGE, 100);
+    }
+
+    private static void configureLaser() {
+        try {
+            LASER_CAN.setRangingMode(LASER_CAN_RANGING_MODE);
+            LASER_CAN.setRegionOfInterest(LASER_CAN_REGION_OF_INTEREST);
+            LASER_CAN.setTimingBudget(LASER_CAN_TIMING_BUDGET);
+        } catch (ConfigurationFailedException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
     public enum GripperState {
