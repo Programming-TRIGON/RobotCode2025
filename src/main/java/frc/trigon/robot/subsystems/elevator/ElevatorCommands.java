1f3a6d028ddccfb2423070e593d5b4524167c929
ShmayaR <shmaya.rosenblatt@gmail.com>
2025-01-12 21:35:16 +0200

    Update ElevatorCommands.java

--- src/main/java/frc/trigon/robot/subsystems/elevator/ElevatorCommands.java
+++ src/main/java/frc/trigon/robot/subsystems/elevator/ElevatorCommands.java
@@ -3,12 +3,12 @@ package frc.trigon.robot.subsystems.elevator;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.StartEndCommand;
 import frc.trigon.robot.RobotContainer;
+import org.trigon.commands.NetworkTablesCommand;
 
-  /*public static Command setDebuggingCommand(){
-        return new NetworkTablesCommand(
-
-        )
-    }*/
+public static Command setDebuggingCommand() {
+    return new NetworkTablesCommand(
+    );
+}
 
 public class ElevatorCommands {
     public static Command getSetTargetStateCommand(ElevatorConstants.ElevatorState targetState) {
