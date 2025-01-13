2d495a5c63242316b845ae47be10fac374df005b
ShmayaR <shmaya.rosenblatt@gmail.com>
2025-01-13 16:49:23 +0200

    Update ElevatorCommands.java

--- src/main/java/frc/trigon/robot/subsystems/elevator/ElevatorCommands.java
+++ src/main/java/frc/trigon/robot/subsystems/elevator/ElevatorCommands.java
@@ -10,7 +10,7 @@ public class ElevatorCommands {
         return new NetworkTablesCommand(
                 ElevatorCommands::getSetTargetPositionCommand,
                 false,
-                ""
+                "Debugging/TargetElevatorPositionMeters"
         );
     }
 
