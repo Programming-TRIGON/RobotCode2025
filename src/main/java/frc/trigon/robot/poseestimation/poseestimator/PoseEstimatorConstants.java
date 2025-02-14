0ab6e687921c8509c4df3bde6144a211cebb18d4
Yishai Levy <96019039+levyishai@users.noreply.github.com>
2025-02-14 08:25:37 +0200

    remove ^1

--- src/main/java/frc/trigon/robot/poseestimation/poseestimator/PoseEstimatorConstants.java
+++ src/main/java/frc/trigon/robot/poseestimation/poseestimator/PoseEstimatorConstants.java
@@ -3,7 +3,7 @@ package frc.trigon.robot.poseestimation.poseestimator;
 public class PoseEstimatorConstants {
     public static final double ODOMETRY_FREQUENCY_HERTZ = 250;
 
-    static final StandardDeviations ODOMETRY_STANDARD_DEVIATIONS = new StandardDeviations(Math.pow(0.003, 1), Math.pow(0.0002, 1));
+    static final StandardDeviations ODOMETRY_STANDARD_DEVIATIONS = new StandardDeviations(0.003, 0.0002);
     static final double
             MAXIMUM_TRANSLATION_VELOCITY_FOR_RELATIVE_ROBOT_POSE_SOURCE_OFFSET_RESETTING_METERS_PER_SECOND = 0,
             MAXIMUM_THETA_VELOCITY_FOR_RELATIVE_ROBOT_POSE_SOURCE_OFFSET_RESETTING_RADIANS_PER_SECOND = 0;
