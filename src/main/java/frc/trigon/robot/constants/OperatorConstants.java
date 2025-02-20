0578cd49846f847c5a31da0a7deb0ad648e9b977
Yishai Levy <96019039+levyishai@users.noreply.github.com>
2025-02-20 23:26:11 +0200

    VERY IMPORTANT FIX

--- src/main/java/frc/trigon/robot/constants/OperatorConstants.java
+++ src/main/java/frc/trigon/robot/constants/OperatorConstants.java
@@ -7,8 +7,7 @@ import org.trigon.hardware.misc.XboxController;
 
 public class OperatorConstants {
     public static final double DRIVER_CONTROLLER_DEADBAND = 0.07;
-    private static final int
-            DRIVER_CONTROLLER_PORT = 0;
+    private static final int DRIVER_CONTROLLER_PORT = 0;
     private static final int DRIVER_CONTROLLER_EXPONENT = 2;
     public static final XboxController DRIVER_CONTROLLER = new XboxController(
             DRIVER_CONTROLLER_PORT, DRIVER_CONTROLLER_EXPONENT, DRIVER_CONTROLLER_DEADBAND
