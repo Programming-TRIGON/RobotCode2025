fe4e1f3cc5bfcef4b2345ddc6dc493dec9a77aee
Strflightmight09 <148347057+Strflightmight09@users.noreply.github.com>
2025-02-24 13:26:13 +0200

    Decided on LED colours

--- src/main/java/frc/trigon/robot/constants/LEDConstants.java
+++ src/main/java/frc/trigon/robot/constants/LEDConstants.java
@@ -10,17 +10,17 @@ import org.trigon.hardware.misc.leds.LEDStripAnimationSettings;
 
 public class LEDConstants {
     public static final LEDStripAnimationSettings.ColorFlowSettings
-            DEFAULT_SETTINGS = new LEDStripAnimationSettings.ColorFlowSettings(Color.kGreen, 0.1, false),
-            MANUAL_GROUND_INTAKE_SETTINGS = new LEDStripAnimationSettings.ColorFlowSettings(Color.kCrimson, 0.7, false),
-            AUTO_GROUND_INTAKE_WITHOUT_VISIBLE_CORAL_SETTINGS = new LEDStripAnimationSettings.ColorFlowSettings(Color.kLightBlue, 0.7, false),
-            AUTO_GROUND_INTAKE_WITH_VISIBLE_CORAL_SETTINGS = new LEDStripAnimationSettings.ColorFlowSettings(Color.kDarkMagenta, 0.7, false);
-    public static final LEDStripAnimationSettings.AlternateColorSettings
-            CORAL_STATION_INTAKE_SETTINGS = new LEDStripAnimationSettings.AlternateColorSettings(Color.kDarkRed, Color.kDeepSkyBlue);
+            DEFAULT_SETTINGS = new LEDStripAnimationSettings.ColorFlowSettings(Color.kDarkGreen, 0.1, false),
+            MANUAL_GROUND_INTAKE_SETTINGS = new LEDStripAnimationSettings.ColorFlowSettings(Color.kDarkMagenta, 0.5, false),
+            AUTO_GROUND_INTAKE_WITHOUT_VISIBLE_CORAL_SETTINGS = new LEDStripAnimationSettings.ColorFlowSettings(Color.kRed, 0.3, false),
+            AUTO_GROUND_INTAKE_WITH_VISIBLE_CORAL_SETTINGS = new LEDStripAnimationSettings.ColorFlowSettings(Color.kDeepSkyBlue, 0.6, false),
+            CORAL_STATION_INTAKE_SETTINGS = new LEDStripAnimationSettings.ColorFlowSettings(Color.kYellow, 0.4, true);
     public static final LEDStripAnimationSettings.BreatheSettings
-            SCORE_CORAL_SETTINGS = new LEDStripAnimationSettings.BreatheSettings(Color.kGreen, 5, 0.3, false, LarsonAnimation.BounceMode.Back);
+            SCORE_CORAL_SETTINGS = new LEDStripAnimationSettings.BreatheSettings(Color.kGreen, 5, 0.2, false, LarsonAnimation.BounceMode.Back),
+            SCORE_CORAL_AUTONOMOUSLY_SETTINGS = new LEDStripAnimationSettings.BreatheSettings(Color.kNavy, 7, 0.4, false, LarsonAnimation.BounceMode.Center);
     public static final LEDStripAnimationSettings.BlinkSettings
-            INTAKE_CONFIRMATION_SETTINGS = new LEDStripAnimationSettings.BlinkSettings(Color.kCrimson, 0.5),
-            RELEASE_CORAL_SETTINGS = new LEDStripAnimationSettings.BlinkSettings(Color.kGreen, 0.75);
+            INTAKE_CONFIRMATION_SETTINGS = new LEDStripAnimationSettings.BlinkSettings(Color.kDarkMagenta, 1),
+            RELEASE_CORAL_SETTINGS = new LEDStripAnimationSettings.BlinkSettings(Color.kGreen, 1);
 
     private static final int CANDLE_ID = 0;
     static final CANdle CANDLE = new CANdle(CANDLE_ID, RobotConstants.CANIVORE_NAME);
