3b48016422569c77e3b9211db619e20933bb9a8c
Nummun14 <142012009+Nummun14@users.noreply.github.com>
2025-03-09 17:43:50 +0200

    :(

--- src/main/java/frc/trigon/robot/poseestimation/apriltagcamera/AprilTagCamera.java
+++ src/main/java/frc/trigon/robot/poseestimation/apriltagcamera/AprilTagCamera.java
@@ -58,7 +58,7 @@ public class AprilTagCamera {
     }
 
     public double getLatestResultTimestampSeconds() {
-        return inputs.latestResultTimestampSeconds;
+        return inputs.latestResultTimestampSeconds - 0.115;
     }
 
     public boolean hasValidResult() {
