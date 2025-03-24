33f6edd430b8181fc32a9be7457bf19a1d725bb4
Yishai Levy <96019039+levyishai@users.noreply.github.com>
2025-03-24 14:16:41 +0200

    "Do a commit from my computer"

--- src/main/java/frc/trigon/robot/subsystems/coralintake/CoralIntakeConstants.java
+++ src/main/java/frc/trigon/robot/subsystems/coralintake/CoralIntakeConstants.java
@@ -160,7 +160,7 @@ public class CoralIntakeConstants {
 
     static final Rotation2d ANGLE_TOLERANCE = Rotation2d.fromDegrees(1.5);
     static final double
-            PULSING_ON_PERIOD_SECONDS = 0.2,
+            PULSING_ON_PERIOD_SECONDS = 0.22,
             PULSING_OFF_PERIOD_SECONDS = 0.1,
             PULSING_INTAKE_MOTOR_VOLTAGE = 8,
             PULSING_FUNNEL_MOTOR_VOLTAGE = CoralIntakeState.COLLECT_FROM_FLOOR.targetFunnelVoltage,
@@ -287,7 +287,7 @@ public class CoralIntakeConstants {
         LOAD_CORAL_TO_GRIPPER_NOT_SEEING_GAME_PIECE_WITH_BEAM_BREAK(-4.5, 0, LOAD_CORAL_TO_GRIPPER_SEEING_GAME_PIECE_WITH_BEAM_BREAK.targetAngle),
         UNLOAD_CORAL_FROM_GRIPPER(6, 2, Rotation2d.fromDegrees(141)),
         CENTER_CORAL(8, 2, LOAD_CORAL_TO_GRIPPER_SEEING_GAME_PIECE_WITH_BEAM_BREAK.targetAngle),
-        COLLECT_FROM_FLOOR(7, 4, Rotation2d.fromDegrees(-46)),
+        COLLECT_FROM_FLOOR(7, 5, Rotation2d.fromDegrees(-46)),
         COLLECT_FROM_FEEDER(6, COLLECT_FROM_FLOOR.targetFunnelVoltage, Rotation2d.fromDegrees(90)),
         EJECT(-3, -1, Rotation2d.fromDegrees(45)),
         REST(0, 0, LOAD_CORAL_TO_GRIPPER_SEEING_GAME_PIECE_WITH_BEAM_BREAK.targetAngle),
