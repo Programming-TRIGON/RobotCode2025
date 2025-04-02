c59001106ab6e8025a03fc9f83705792fc9fc8dc
Strflightmight09 <148347057+Strflightmight09@users.noreply.github.com>
2025-04-02 14:54:43 +0300

    No time 😪

--- src/main/java/frc/trigon/robot/commands/commandfactories/CoralCollectionCommands.java
+++ src/main/java/frc/trigon/robot/commands/commandfactories/CoralCollectionCommands.java
@@ -2,7 +2,6 @@ package frc.trigon.robot.commands.commandfactories;
 
 import edu.wpi.first.wpilibj2.command.*;
 import frc.trigon.robot.RobotContainer;
-import frc.trigon.robot.commands.CommandConstants;
 import frc.trigon.robot.constants.OperatorConstants;
 import frc.trigon.robot.subsystems.coralintake.CoralIntakeCommands;
 import frc.trigon.robot.subsystems.coralintake.CoralIntakeConstants;
@@ -41,8 +40,8 @@ public class CoralCollectionCommands {
     private static Command getInitiateFloorCoralCollectionCommand() {
         return new ParallelCommandGroup(
                 CoralIntakeCommands.getSetTargetStateCommand(CoralIntakeConstants.CoralIntakeState.COLLECT_FROM_FLOOR),
-                getScheduleCoralLoadingWhenCollectedCommand(),
-                CommandConstants.FIELD_RELATIVE_DRIVE_WITH_JOYSTICK_ORIENTED_ROTATION_COMMAND
+                getScheduleCoralLoadingWhenCollectedCommand()
+//                CommandConstants.FIELD_RELATIVE_DRIVE_WITH_JOYSTICK_ORIENTED_ROTATION_COMMAND
         );
     }
 
